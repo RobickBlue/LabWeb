7b3680f3a4325d918a04a0d906296474f9adbe77
Rodrigo Martinez Jacobson <dimes23@gmail.com>
2016-01-17 10:24:23 +0100

    Categorias generadas dinamicamente
    - Se consulta a la BBDD y luego se genera el menu de categorias

--- Presentacion/src/_nav.php
+++ Presentacion/src/_nav.php
@@ -1,14 +1,20 @@
+<?php include("bean.php") ?>
 <div class="container">
   <div class="row">
     <div class="col-md-5" id="productos">
       <ul>
         <li id="index">Inicio</li>
         <li id="produc">Productos
-          <ul id="desp" style="display: none;">
-            <a href="controlador.php?id_categoria=1"><li >Arduino</li></a>
-            <a href="controlador.php?id_categoria=2"><li >BeagleBone</li></a>
-            <a href="controlador.php?id_categoria=3"><li >Raspberry Pi</li></a>
-            <a href="controlador.php?id_categoria=4"><li >Sensores</li></a>
+          <ul id="desp">
+            <?php
+                $Bean = bean::getInstance();
+                $categorias = $Bean->getCategorias();
+                foreach($categorias as $categoria) {
+                  $nom = $categoria['DESCCATEGORIA'];
+                  $id = $categoria['IDCATEGORIA']
+            ?>
+            <a href="controlador.php?id_categoria=<?php echo $id; ?>"><li ><?php echo $nom; ?></li></a>
+            <?php } ?>
           </ul>
         </li>
     </ul>
@@ -19,7 +25,6 @@
       <li id="login">Mi cuenta</li>
       <li><img src="..\img\carrito.png" width="30%" height="30%">Cesta</li>
   </ul>
-</div>
-  </div>
+    </div>
   </div>
 </div>
--- Presentacion/src/_navLogin.php
+++ Presentacion/src/_navLogin.php
@@ -1,3 +1,4 @@
+<?php include("bean.php") ?>
 <div class="container">
   <div class="row">
     <div class="col-md-5" id="productos">
@@ -5,10 +6,15 @@
         <li id="index">Inicio</li>
         <li id="produc">Productos
           <ul id="desp" style="display: none;">
-            <a href="controlador.php?id_categoria=1"><li >Arduino</li></a>
-            <a href="controlador.php?id_categoria=2"><li >BeagleBone</li></a>
-            <a href="controlador.php?id_categoria=3"><li >Raspberry Pi</li></a>
-            <a href="controlador.php?id_categoria=4"><li >Sensores</li></a>
+            <?php
+            $Bean = bean::getInstance();
+            $categorias = $Bean->getCategorias();
+            foreach($categorias as $categoria) {
+              $nom = $categoria['DESCCATEGORIA'];
+              $id = $categoria['IDCATEGORIA']
+              ?>
+              <a href="controlador.php?id_categoria=<?php echo $id; ?>"><li ><?php echo $nom; ?></li></a>
+            <?php } ?>
           </ul>
         </li>
     </ul>
